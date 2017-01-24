f960e188de8a2bd54f17bdfbe28e7f67e85d5c30
nami634 <cigarbox.nami@gmail.com>
2017-01-24 18:29:43 +0900

    create new file

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,28 @@ class NotesController < ApplicationController
   def index
 
   end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @
+  end
+
+  def edit
+
+  end
+
+  def update
+    
+  end
+
+  def destroy
+
+  end
+
+  def show
+    
+  end
 end
